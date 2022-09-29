6fd5c6262083c48995135e46db6aa2a5c5a6d76b
ethanjonm <ethanjonmercado@gmail.com>
2022-09-29 19:23:30 -0400

    day 4

--- w1d4.js
+++ w1d4.js
@@ -14,3 +14,46 @@ console.log(arr[0]);
 console.log(arr[6]);
 console.log(arr[6][1]) //we go into the second array in the log
 
+console.log("---2")
+
+let fruits = ['apple', 'orange', 'pear'];
+fruits.length; //3
+fruits.indexOf('pear') //2
+fruits.includes('watermelon') // false
+fruits.push('watermelon') // added to the array from the end
+console.log(fruits)
+fruits.pop() // remove from the end of the array
+console.log(fruits)
+fruits.unshift('grape'); // added to the array from the front
+console.log(fruits)
+fruits.shift();  // removes from the front
+console.log(fruits)
+
+console.log("---3")
+
+let numbers = [1,2,3]
+console.log(fruits.concat(numbers)) //concat does not mutate it will return new array
+console.log(fruits)
+console.log(numbers.slice()); // no arguemnt gives a copy of array
+console.log(numbers.slice(1)); // starts from given index to end of array
+console.log(numbers.slice(0,2)); //(0) inclusive (2) exclusive
+
+console.log("---4")
+
+let string = "hi my name is Xander";
+console.log(string.split(" ")); // turn string into array
+console.log(string);
+
+console.log(numbers.join("___"))
+
+for(let i = 0; i < numbers.length; i++){
+    let ele = numbers [i];
+    console.log(ele);
+}
+
+for (let i = numbers.lenghts -1; i >= 0; i--){
+    let ele = numbers [i];
+    console.log(ele)
+}
+
+
