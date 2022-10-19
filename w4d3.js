a36141ece5811661b278d85eafaac3c6cd16c696
ethanjonm <ethanjonmercado@gmail.com>
2022-10-19 18:43:10 -0400

    week4 day 3

---
+++ w4d3.js
@@ -0,0 +1,21 @@
+/*
+Recursion
+- a function that calls itself withen itself
+- A base case
+- A recursive case
+- Whatever can be done recursively CAN be done iteratively
+why do we care?
+-other professionals use it
+-elegance**
+*/
+
+
+// function multiply(num1, num2) {
+//     //if (num1 === 0) return 0  // if you dont have this base case you get a stack overflow
+//     console.log(num1)
+//     return num2 + multiply(num1 - 1, num2)
+    
+// }
+// console.log(multiply(2,3))
+// console.log(multiply(7,8))
+
